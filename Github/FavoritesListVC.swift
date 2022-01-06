62b39a8ecb9d3cce52b6dd9cff18224045698d81
Kevin Hankes <darthonasi@Kevins-MacBook-Pro.local>
2022-01-06 17:59:12 -0600

    Create reusable button and text field and add to searchVC

---
+++ Github/FavoritesListVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesListViewController.swift
+//  Github
+//
+//  Created by Kevin Hankes on 1/3/22.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor =  .systemBlue
+    }
+}
