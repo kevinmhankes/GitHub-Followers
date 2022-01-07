12c8d92f50368e2aa444c8609e4032bb1bb40bcf
Kevin Hankes <48036731+kevinmhankes@users.noreply.github.com>
2022-01-06 18:23:42 -0600

    Pass data from SearchVC to FollowerListVC and add keyboard controls

---
+++ Github/FollowerListVC.swift
@@ -0,0 +1,20 @@
+//
+//  FollowerListVC.swift
+//  Github
+//
+//  Created by Kevin Hankes on 1/6/22.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+
+    var username: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.isNavigationBarHidden = false
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+}
