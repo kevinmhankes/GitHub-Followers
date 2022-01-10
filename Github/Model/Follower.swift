8320074319f9cfef7a87d887f53d09ba918130ac
Kevin Hankes <48036731+kevinmhankes@users.noreply.github.com>
2022-01-09 19:37:38 -0600

    Add basic networking and error handling

---
+++ Github/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  Github
+//
+//  Created by Kevin Hankes on 1/9/22.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+
+}
--- Github/Screens/FollowerListVC.swift
+++ Github/Screens/FollowerListVC.swift
@@ -16,5 +16,21 @@ class FollowerListVC: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.isNavigationBarHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { (followers, errorMessage) in
+            guard let followers = followers else {
+                self.presentGHAlertOnMainThread(title: "Bad Stuff Happened", message: errorMessage!, buttonTitle: "Ok")
+                return
+            }
+            
+            print("Followers.count = \(followers.count)")
+            print(followers)
+        }
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
     }
 }
