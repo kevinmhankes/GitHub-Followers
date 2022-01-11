9153133c9ffac5af0a9ab04eb0cd6f26a9362770
Kevin Hankes <48036731+kevinmhankes@users.noreply.github.com>
2022-01-10 19:23:52 -0600

    Download and cache avatar images

--- Github/Managers/NetworkManager.swift
+++ Github/Managers/NetworkManager.swift
@@ -5,11 +5,12 @@
 //  Created by Kevin Hankes on 1/9/22.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     static let shared = NetworkManager()
     let baseURL = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
     
     private init() {}
     
