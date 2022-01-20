bec103097b54274ab9db27f38b262da36fc283cf
Kevin Hankes <48036731+kevinmhankes@users.noreply.github.com>
2022-01-20 17:21:58 -0600

    Add date extensions and display date label

---
+++ Github/Extensions/Date+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  Date+Ext.swift
+//  Github
+//
+//  Created by Kevin Hankes on 1/20/22.
+//
+
+import Foundation
+
+extension Date {
+    
+    func convertToMonthYearFormat() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM yyyy"
+        
+        return dateFormatter.string(from: self)
+    }
+}
