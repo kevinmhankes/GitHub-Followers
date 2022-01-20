4b3cc7b01b0b10ca47c07102a8adbf64c3a795be
Kevin Hankes <48036731+kevinmhankes@users.noreply.github.com>
2022-01-20 16:47:16 -0600

    Create item info view controller to get generic view set up

---
+++ Github/Custom Views/ViewControllers/GHItemInfoVC.swift
@@ -0,0 +1,58 @@
+//
+//  GHItemInfoVC.swift
+//  Github
+//
+//  Created by Kevin Hankes on 1/20/22.
+//
+
+import UIKit
+
+class GHItemInfoVC: UIViewController {
+    
+    let stackView = UIStackView()
+    let itemInfoViewOne = GHItemInfoView()
+    let itemInfoViewTwo = GHItemInfoView()
+    let actionButton = GHButton()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureBackgroundView()
+        layoutUI()
+        configureStackView()
+    }
+    
+    private func configureBackgroundView() {
+        view.layer.cornerRadius = 18
+        view.backgroundColor = .secondarySystemBackground
+        
+    }
+    
+    private func configureStackView() {
+        stackView.axis = .horizontal
+        stackView.distribution = .equalSpacing
+        
+        stackView.addArrangedSubview(itemInfoViewOne)
+        stackView.addArrangedSubview(itemInfoViewTwo)
+    }
+    
+    private func layoutUI() {
+        view.addSubview(stackView)
+        view.addSubview(actionButton)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        let padding: CGFloat = 20
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            stackView.heightAnchor.constraint(equalToConstant: 50),
+            
+            actionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -padding),
+            actionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            actionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            actionButton.heightAnchor.constraint(equalToConstant: 44)
+        ])
+    }
+
+}
