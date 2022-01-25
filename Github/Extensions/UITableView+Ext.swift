adef3cac294a8cf745c0bc63690dfddf7e818889
Kevin Hankes <48036731+kevinmhankes@users.noreply.github.com>
2022-01-25 15:08:16 -0600

    Add UITableView extension and fix for favorites list delete

---
+++ Github/Extensions/UITableView+Ext.swift
@@ -0,0 +1,19 @@
+//
+//  UITableView+Ext.swift
+//  Github
+//
+//  Created by Kevin Hankes on 1/25/22.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    func reloadDataOnMainThread() {
+        DispatchQueue.main.async { self.reloadData() }
+    }
+    
+    func removeExcessCells() {
+        tableFooterView = UIView(frame: .zero)
+    }
+}
